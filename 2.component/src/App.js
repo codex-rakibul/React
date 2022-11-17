e2828c29938acb8fc440c824790497b349437a29
rakibul667s <rakibulislam667s@gmail.com>
2022-11-17 15:09:29 +0600

    Component

---
+++ 2.component/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>React Application</h1>
+       <Person></Person>
+       <Pra></Pra>
+      </header>
+    </div>
+  );
+}
+
+function Person() {
+  return (
+    <div>
+      <h3>Rakibul Islam Sony</h3>
+      <p>id: 01319106062 </p>
+    </div>
+  );
+}
+
+function Pra(){
+  return (
+    <div>
+      <p>Hey, I'm a Pra</p>
+    </div>
+  );
+}
+
+export default App;
